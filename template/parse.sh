db9ca65019a9a1be2ad7cc2619e777d41716912a
Howin Ho <ho1ho@yahoo.com>
2020-01-16 09:00:03 +0800

    add function to process mustaches files

--- template/parse.sh
+++ template/parse.sh
@@ -7,11 +7,32 @@ if [ $# -eq 1 ]; then
     view="./_views/$1.json"
 fi
 
+# Handle '*.mustache' files
 files="find . -name '*.mustache' -print"
 for infile in `eval ${files}`; do
     echo "Processing ${infile}..."
     outfile=$(echo .${infile} | sed 's/\(.*\)\.mustache/\1/')
     mustache ${view} ${infile} > ${outfile}
+done
+
+# Handle '*.mustaches' files - which split the file into few files
+files="find . -name '*.mustaches' -print"
+for infile in `eval ${files}`; do
+
+    # Mustache the file
+    echo "Processing ${infile}..."
+    outfile=$(echo .${infile} | sed 's/\(.*\)\.mustaches/\1/')
+    mustache ${view} ${infile} > ${outfile}
+
+    # Split the file name with '.'
+    filnam=$(basename ${outfile} | sed 's/\.[^.]*$//')
+    filext=$(echo ${outfile} | sed 's/.*\.//')
+
+    # Split the file into multiple files
+    awk -v filnam=${filnam} -v filext=${filext} '/--SPLIT--/ {x = sprintf(filnam"%s%s", ++i, "."filext); next} {print > x}' ${outfile}
+
+    # Remove the intermediate out file
+    rm ${outfile}
 done
 
 echo "Done!"
