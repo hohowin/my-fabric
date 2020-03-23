0280e4202f351d9f1b516378dbb3554d0c1db906
Howin Ho <ho1ho@yahoo.com>
2020-03-23 15:26:09 +0800

    fix cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -7,9 +7,9 @@ docker system prune -f
 # Cleanup artifacts
 case $(uname -s) in
     Darwin)
-        rm -rf config/../artifacts;;
+        rm -rf network/config/../artifacts;;
    *)
-        sudo rm -rf config/../artifacts;;
+        sudo rm -rf network/config/../artifacts;;
 esac
 
 # Cleanup chaincode images
@@ -18,6 +18,10 @@ docker rmi -f $(docker images | grep eventstore | awk '{print $3}')
 docker rmi -f $(docker images | grep privatedata | awk '{print $3}')
 
 # Cleanup chaincode builds
+export _CHAINCODE_DIR=chaincode
 rm -rf ${_CHAINCODE_DIR}/fabcar/dist
 rm -rf ${_CHAINCODE_DIR}/fabcar/node_modules
 rm -f ${_CHAINCODE_DIR}/fabcar/yarn.lock
+rm -rf ${_CHAINCODE_DIR}/op/dist
+rm -rf ${_CHAINCODE_DIR}/op/node_modules
+rm -f ${_CHAINCODE_DIR}/op/yarn.lock
